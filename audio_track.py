047369d927aead57affc32362c3862f9c5276288
MaximHunko <maxim.hunko@developex.net>
2023-04-04 16:49:57 +0300

    Added filter for searching by parameters.

--- audio_track.py
+++ audio_track.py
@@ -5,7 +5,6 @@ from dev_tools import get_file_names_in_folder
 class AudioTrack:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.file_type = 'mp3'
         self.audio = MP3(file_path)
         self.title = self.audio['TIT2'].text[0] if 'TIT2' in self.audio else None
         self.artist = self.audio['TPE1'].text[0] if 'TPE1' in self.audio else None
@@ -15,12 +14,66 @@ class AudioTrack:
         self.year = self.audio['TDRC'].text[0] if 'TDRC' in self.audio else None
         self.track_number = self.audio['TRCK'].text[0] if 'TRCK' in self.audio else None
 
+
+class AudioTrackFilter:
+    def __init__(self):
+        self.title = None
+        self.title_part = True
+        self.artist = None
+        self.artist_part = True
+        self.genre = None
+        self.genre_part = True
+        self.year_ignore = True
+        self.year_min = 0
+        self.year_max = 3000
+
+    def _check_title(self, audio_track):
+        if not self.title:
+            return True
+        if self.title_part:
+            return audio_track.title in self.title
+        return self.title == audio_track.title
+
+    def _check_artist(self, audio_track):
+        if not self.artist:
+            return True
+        if self.artist_part:
+            return audio_track.artist in self.artist
+        return self.artist == audio_track.artist
+
+    def _check_genre(self, audio_track):
+        if not self.genre:
+            return True
+        if self.genre_part:
+            return audio_track.genre in self.genre
+        return self.genre == audio_track.genre
+
+    def __eq__(self, other):
+        if isinstance(other, AudioTrackFilter):
+            return (not self.title or self.title == other.title) and \
+                (not self.title_part or self.title_part == other.title_part) and \
+                (not self.artist or self.artist == other.artist) and \
+                (not self.artist_part or self.artist_part == other.artist_part) and \
+                (not self.genre or self.genre == other.genre) and \
+                (not self.year_min or self.year_min == other.year_min) and \
+                (not self.year_max or self.year_max == other.year_max)
+        elif isinstance(other, AudioTrack) or isinstance(other, AudioTrackMp3):
+            return self._check_title(other) and self._check_artist(other) and self._check_genre(other) and \
+                (self.year_ignore or self.year_max > other.get_year_safe() > self.year_min)
+        return False
+
+
+class AudioTrackMp3(AudioTrack):
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.file_type = 'mp3'
+
     def get_year_safe(self):
         try:
             y = int(str(self.year))
             return y
         except ValueError:
-            return None
+            return -1
 
 
 folder_path = input('Please, enter path to the folder with yours mp3 files: ')
@@ -39,13 +92,15 @@ print('Indexing')
 
 for mp3_file in mp3_files:
     print('...............')
-    audio_tracks_list.append(AudioTrack(f'{folder_path}\\{mp3_file}'))
+    audio_tracks_list.append(AudioTrackMp3(f'{folder_path}\\{mp3_file}'))
 
 item_index = 0
 
+filter_mp3 = AudioTrackFilter()
+filter_mp3.artist = 'Queen'
+
 for song in audio_tracks_list:
-    year = song.get_year_safe()
-    if year and year < 1990 and song.genre == 'Rock':
+    if filter_mp3 == song:
         print("-----------------------------------------")
         print(f"Track    № {item_index + 1}")
         print("Title:  ", song.title)
@@ -56,4 +111,3 @@ for song in audio_tracks_list:
         print("Year    ", song.year)
         print("")
         item_index += 1
-
