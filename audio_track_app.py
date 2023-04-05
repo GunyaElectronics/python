d1bc4e248eaf4c1734120eac26bc2887f5e47474
MaximHunko <maxim.hunko@developex.net>
2023-04-05 11:55:23 +0300

    Move read of the folder in other thread to unlock window thread when read_folder button clicked.

--- audio_track_app.py
+++ audio_track_app.py
@@ -1,12 +1,18 @@
 from dev_tools import get_file_names_in_folder
 import tkinter as tk
 from tkinter import *
+from tkinter.ttk import Progressbar
 from audio_track import *
+import threading
 
 
 class App:
     def __init__(self, master):
         self.master = master
+
+        self.progress_bar = Progressbar(root, orient=HORIZONTAL, length=200, mode='determinate')
+        self.progress_bar['value'] = 0
+
         self.listbox = tk.Listbox(self.master, width=100, height=30)
         self.listbox.pack(fill=BOTH, expand=True)
 
@@ -17,34 +23,45 @@ class App:
         self.scrollbar.config(command=self.listbox.yview)
 
         self.button_read = tk.Button(self.master, text="Read folder", command=self.click_button_read)
-        self.button_read.pack(side=RIGHT)
 
         self.label = tk.Label(self.master, text='Enter folder path:')
         self.label.pack(side=LEFT)
 
         self.entry = tk.Entry(self.master, width=40)
         self.entry.pack(side=LEFT)
+        self.button_read.pack(side=LEFT)
+        self.progress_bar.pack(pady=10)
 
     def click_button_read(self):
-        folder_path = self.entry.get()
-        # Search all mp3 files in folder
-        mp3_files = get_file_names_in_folder(folder_path, 'mp3')
+        def read_folder(app_self):
+            folder_path = app_self.entry.get()
+            # Search all mp3 files in folder
+            mp3_files = get_file_names_in_folder(folder_path, 'mp3')
 
-        count_of_files = len(mp3_files)
-        if count_of_files == 0:
-            self.listbox.insert(tk.END, 'No MP3 files found')
+            count_of_files = len(mp3_files)
+            if count_of_files == 0:
+                app_self.listbox.insert(tk.END, 'No MP3 files found')
 
-        audio_tracks_list = []
+            audio_tracks_list = []
 
-        for mp3_file in mp3_files:
-            audio_tracks_list.append(AudioTrackMp3(f'{folder_path}\\{mp3_file}'))
+            file_index = 0
+            for mp3_file in mp3_files:
+                percent = file_index * 100 / count_of_files
+                if app_self.progress_bar['value'] != percent:
+                    app_self.progress_bar['value'] = percent
+                audio_tracks_list.append(AudioTrackMp3(f'{folder_path}\\{mp3_file}'))
+                file_index += 1
 
-        filter_mp3 = AudioTrackFilter()
-        # filter_mp3.artist = 'Queen'
+            app_self.progress_bar['value'] = 100
 
-        for song in audio_tracks_list:
-            if filter_mp3 == song:
-                self.listbox.insert(tk.END, f'{song.artist} - {song.title}')
+            filter_mp3 = AudioTrackFilter()
+
+            for song in audio_tracks_list:
+                if filter_mp3 == song:
+                    app_self.listbox.insert(tk.END, f'{song.artist} - {song.title}')
+
+        thread = threading.Thread(target=read_folder, args=(self,))
+        thread.start()
 
 
 root = tk.Tk()
