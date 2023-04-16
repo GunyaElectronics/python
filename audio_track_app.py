19af1b8c03a48ec2fe7504ae19f588dcf903b210
GunyaElectronics <gunya.electronics@gmail.com>
2023-04-16 13:02:08 +0300

    Updated UI structure, added two frames for left side pack of frames and bottom side pack of the elements

--- audio_track_app.py
+++ audio_track_app.py
@@ -16,10 +16,17 @@ class App:
 
         # UI elements
         self.master = master_ui
-        self.progress_bar = Progressbar(self.master, orient=HORIZONTAL, length=200, mode='determinate')
+
+        self.visual_data_frame = tk.Frame(self.master)
+        self.visual_data_frame.pack(side=LEFT, fill=BOTH, expand=True)
+
+        self.buttons_frame = tk.Frame(self.master)
+        self.buttons_frame.pack()
+
+        self.progress_bar = Progressbar(self.visual_data_frame, orient=HORIZONTAL, length=200, mode='determinate')
         self.progress_bar['value'] = 0
 
-        self.listbox = tk.Listbox(self.master, width=100, height=30)
+        self.listbox = tk.Listbox(self.visual_data_frame, width=100, height=30)
 
         self.scrollbar = Scrollbar(self.listbox)
         self.scrollbar.pack(side=RIGHT, fill=Y)
@@ -28,11 +35,11 @@ class App:
         self.scrollbar.config(command=self.listbox.yview)
 
         elements_width = 33
-        self.button_read = tk.Button(self.master, text="Read folder content", command=self.click_button_read,
+        self.button_read = tk.Button(self.buttons_frame, text="Read folder content", command=self.click_button_read,
                                      width=elements_width)
-        self.button_apply = tk.Button(self.master, text="Apply filter", command=self.click_button_apply,
+        self.button_apply = tk.Button(self.buttons_frame, text="Apply filter", command=self.click_button_apply,
                                       width=elements_width)
-        self.button_sort = tk.Button(self.master, text="Sort songs", command=self.click_button_sort,
+        self.button_sort = tk.Button(self.buttons_frame, text="Sort songs", command=self.click_button_sort,
                                      width=elements_width)
         self.file_types_dict = {'Search all files': ['mp3', 'flac'],
                                 'Search mp3 only': ['mp3'],
@@ -43,22 +50,22 @@ class App:
         self.selected_file_option = tk.StringVar(value=self.file_type_options[0])
 
         self.selected_file_option.trace('w', self.file_type_option_changed)
-        self.option_selected_file_menu = tk.OptionMenu(self.master, self.selected_file_option, *self.file_type_options)
+        self.option_selected_file_menu = tk.OptionMenu(self.buttons_frame, self.selected_file_option, *self.file_type_options)
         self.option_selected_file_menu.config(width=elements_width)
 
         self.sort_by_options = ['Artist', 'Title', 'Genre', 'Album']
         self.sort_by = self.sort_by_options[0].lower()
         self.selected_sort_by_option = tk.StringVar(value=self.sort_by_options[0])
         self.selected_sort_by_option.trace('w', self.sort_by_option_changed)
-        self.option_sort_by_menu = tk.OptionMenu(self.master, self.selected_sort_by_option, *self.sort_by_options)
+        self.option_sort_by_menu = tk.OptionMenu(self.buttons_frame, self.selected_sort_by_option, *self.sort_by_options)
         self.option_sort_by_menu.config(width=elements_width)
 
         entry_width = 40
-        self.entry_path = tk.Entry(self.master, width=entry_width)
-        self.entry_artist = tk.Entry(self.master, width=entry_width)
-        self.entry_title = tk.Entry(self.master, width=entry_width)
-        self.entry_genre = tk.Entry(self.master, width=entry_width)
-        self.entry_year = tk.Entry(self.master, width=entry_width)
+        self.entry_path = tk.Entry(self.buttons_frame, width=entry_width)
+        self.entry_artist = tk.Entry(self.buttons_frame, width=entry_width)
+        self.entry_title = tk.Entry(self.buttons_frame, width=entry_width)
+        self.entry_genre = tk.Entry(self.buttons_frame, width=entry_width)
+        self.entry_year = tk.Entry(self.buttons_frame, width=entry_width)
 
         # Draw window using pack method
         self.listbox.pack(fill=BOTH, expand=True)
